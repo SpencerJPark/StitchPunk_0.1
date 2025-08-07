42e26d44a8228440db6a4a8b3c523dd83029a41f
SpencerJPark <106630109+SpencerJPark@users.noreply.github.com>
2025-08-06 20:33:11 -0700

    small changes

---
+++ Assets/_Scripts/Data/Enums/RivePanelType.cs
@@ -0,0 +1,13 @@
+// Assets/Scripts/Data/RivePanelType.cs
+namespace Data
+{
+    public enum RivePanelType
+    {
+        UI,
+        Human,
+        CreatureSmall,
+        CreatureLarge,
+        Boss,
+        // …add more as needed
+    }
+}
